d711ac2bebea344262df0a5dd9aeb0568a4079cd
web444 <62865753+web444@users.noreply.github.com>
2022-02-04 09:24:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,101 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+// Create a reference for the canvas
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+    var img_imgTag = new Image(); //defining a variable with a new image
+    img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+    img_imgTag.src = img_image; // load image
+}
+
+function uploadimg() {
+
+    ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Write a code to grab the key-pressed event
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e) {
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+
+    if ((keyPressed >= 97 && keyPressed <= 122) || (keyPressed >= 65 && keyPressed <= 90)) {
+
+        aplhabetkey();
+        document.getElementById("d1").innerHTML = "You Pressed Alphabet Key";
+
+
+    } else {
+
+        if (keyPressed > 47 && keyPressed < 58) {
+
+            numberkey();
+            document.getElementById("d1").innerHTML = "You Pressed Number Key";
+
+        } else {
+
+            if (keyPressed > 36 && keyPressed < 41) {
+
+                arrowkey();
+                document.getElementById("d1").innerHTML = "You Pressed Arrow Key";
+
+            } else {
+
+                if ((keyPressed == 17) || (keyPressed == 18) || (keyPressed == 27)) {
+
+                    specialkey();
+                    document.getElementById("d1").innerHTML = "You Pressed Special(ctrl/alt/esc) Key";
+
+                } else {
+
+                    otherkey();
+                    document.getElementById("d1").innerHTML = "You Pressed other(enter/shift/del) Key";
+
+                }
+
+            }
+
+        }
+
+    }
+}
+
+function aplhabetkey() {
+    //upload respective image with the message. 
+    img_image = "Alpkey.png";
+    add();
+
+}
+
+function numberkey() {
+
+    img_image = "numkey.png";
+    add();
+}
+
+function arrowkey() {
+
+    img_image = "Arrkey.png";
+    add();
+}
+
+function specialkey() {
+    img_image = "spkey.png";
+    add();
+}
+
+function otherkey() {
+    img_image = "otherkey.png";
+    add();
+}
